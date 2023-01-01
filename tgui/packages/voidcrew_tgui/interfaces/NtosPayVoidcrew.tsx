61f2ad94701d4105d42be2fca4195e51809f6986
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-12-31 23:20:43 -0500

    Functional NTPay & Improvements
    NtOS pay app now lets you remove & add people to the bank account

--- tgui/packages/voidcrew_tgui/interfaces/NtosPayVoidcrew.tsx
+++ tgui/packages/voidcrew_tgui/interfaces/NtosPayVoidcrew.tsx
@@ -1,7 +1,7 @@
-import { NtosPay } from '../../tgui/interfaces/NtosPay';
+import { NtosPayContent } from '../../tgui/interfaces/NtosPay';
 import { useBackend, useSharedState } from '../../tgui/backend';
 import { NtosWindow } from '../../tgui/layouts';
-import { Stack, Tabs, Table } from '../../tgui/components';
+import { NoticeBox, Stack, Tabs, Table, Button } from '../../tgui/components';
 
 export const NtosPayVoidcrew = (props, context) => {
   const { data } = useBackend(context);
@@ -34,7 +34,7 @@ export const NtosPayVoidcrew = (props, context) => {
             </Tabs>
           </Stack.Item>
           <Stack.Item grow>
-            {screenmode === NTOS_PAY && <NtosPay />}
+            {screenmode === NTOS_PAY && <NtosPayContent />}
             {screenmode === ALL_ACCOUNTS && <AllAccounts />}
           </Stack.Item>
         </Stack>
@@ -44,20 +44,66 @@ export const NtosPayVoidcrew = (props, context) => {
 };
 
 type Info = {
-  all_accounts: string[];
+  all_accounts: Accounts[];
+  swiped_id: SwipedID[];
+};
+
+type Accounts = {
+  ref: string[];
+  name: string;
+};
+
+type SwipedID = {
+  ref: string[];
+  account: string;
 };
 
 const AllAccounts = (props, context) => {
-  const { data } = useBackend<Info>(context);
-  const { all_accounts } = data;
+  const { act, data } = useBackend<Info>(context);
+  const { all_accounts, swiped_id } = data;
 
+  if (!all_accounts) {
+    return (
+      <NoticeBox>
+        You need to insert your ID card into the card slot in order to use this
+        application.
+      </NoticeBox>
+    );
+  }
   return (
-    <Table>
-      {all_accounts.map((account) => (
-        <Table.Row key={account} className="candystripe">
-          <Table.Cell width="100px">{account}</Table.Cell>
-        </Table.Row>
-      ))}
-    </Table>
+    <>
+      {!!swiped_id && (
+        <Table>
+          {swiped_id.map((id) => (
+            <Table.Row key={id.ref} className="candystripe">
+              <Button
+                icon="card"
+                content={id.account}
+                tooltip="Adds the user to our bank account."
+                onClick={() => act('add_account')}
+              />
+            </Table.Row>
+          ))}
+        </Table>
+      )}
+      <Table>
+        {all_accounts.map((account) => (
+          <Table.Row key={account.ref} className="candystripe">
+            <Table.Cell>{account.name}</Table.Cell>
+            <Button
+              icon="lock"
+              color="caution"
+              content="Remove ID"
+              tooltip="Removes the ID as a user from the bank account"
+              onClick={() =>
+                act('remove_account', {
+                  removed_account: account.ref,
+                })
+              }
+            />
+          </Table.Row>
+        ))}
+      </Table>
+    </>
   );
 };
